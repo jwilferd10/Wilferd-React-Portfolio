c1b837cc7ae2cd9fe15568a2779140f5c51fdaa8
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-04-14 15:42:56 -0700

    Feat: Created a skillset section in resume, styled the page. Moving onto Contact

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,77 @@
 import React from 'react';
+import {Card, Accordion, Button} from 'react-bootstrap'
 
 const Resume = () => {
     return (
+        
         <React.Fragment>
-            <h1>Resume</h1>
+            <div class="about-text">
+                <h1 class="section-title resume">Resume</h1>
+                <h2>Education:</h2>
+                <h3>California Virtual Academy @ Los Angeles</h3>  
+                <p>Class of 2016</p>
+                <h3>UCLA Extension Coding Bootcamp </h3>
+                <p>As of April, 2021</p>
+            </div>
+
+            <h1 class="section-title skills">Skillset</h1>
+
+            <h3 class="about-text">
+                Within my twenty-four weeks in a Coding Bootcamp, I've picked up these following skills throughout the class:
+            </h3>
+            <div class="about-text">
+                <li>
+                    HTML5
+                </li>
+                
+                <li>
+                    CSS
+                </li>
+                
+                <li>
+                    Git
+                </li>
+                
+                <li>
+                    JavaScript
+                </li>
+                
+                <li>
+                    Node.JS
+                </li>
+               
+                <li>
+                    Express.JS
+                </li>
+                
+                <li>
+                    Object-Orientated Programming
+                </li>
+                
+                <li>
+                    Object Relational Mapping
+                </li>
+                
+                <li>
+                    SQL
+                </li>
+               
+                <li>
+                    NoSQL
+                </li>
+                
+                <li>
+                    Handlebars
+                </li>
+                
+                <li>
+                    GraphQL
+                </li>
+                
+                <li>
+                    React
+                </li>
+            </div>
         </React.Fragment>
 
     )
