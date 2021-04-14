8f08900d4e89e35fa491f13ca884f74a244e67ae
Jonathan Wilferd <jwilferd10@yahoo.com>
2021-04-14 16:55:14 -0700

    Feat: More work done, put some work into responsiveness.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,14 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function Footer() {
     return (
         <footer data-component="Footer" id="footer" className="footer section container-fluid">
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Siphon880gh/reprint/" className="text-bold nav-link">Github</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/jwilferd10" className="text-bold nav-link">Github</a>
                     
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Siphon880gh/reprint/" className="text-bold nav-link">LinkedIn</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/jonathan-wilferd-3a6b461b6/" className="text-bold nav-link">LinkedIn</a>
                     
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Siphon880gh/reprint/" className="text-bold nav-link">StackOverflow</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://stackoverflow.com/users/14244734/jonathan-wilferd" className="text-bold nav-link">StackOverflow</a>
         </footer>   
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Form, Button} from 'react-bootstrap'
+import {Form, Button, Card} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.css';
 
 import { validateEmail } from '../utils/helpers';
@@ -41,31 +41,33 @@ function ContactForm() {
   return (
     <section>
         <h1 class="section-title about">Reach Out:</h1>
-        <Form id="contact-form" onSubmit={handleSubmit}>
-            <div>
-                <Form.Group controlId="NameInput">
-                    <Form.Label>Name:</Form.Label>
-                    <Form.Control type="text" name="name" defaultValue={name} onBlur={handleChange}/>
-                </Form.Group>
+        <Card class="contact-container">
+          <Form id="contact-form" onSubmit={handleSubmit}>
+              <div>
+                  <Form.Group controlId="NameInput">
+                      <h3>Name:</h3>
+                      <Form.Control id="nameValue" as="textarea" name="name" defaultValue={name} onBlur={handleChange}/>
+                  </Form.Group>
 
-                <Form.Group controlId="EmailInput">
-                    <Form.Label>Email:</Form.Label>
-                    <Form.Control type="email" name="email" defaultValue={email} onBlur={handleChange} />
-                </Form.Group>
+                  <Form.Group controlId="EmailInput">
+                      <h3>Email:</h3>
+                      <Form.Control id="emailValue" type="email" name="email" defaultValue={email} onBlur={handleChange} />
+                  </Form.Group>
 
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label htmlFor="message">Message:</Form.Label>
-                    <Form.Control as="textarea" name="message" rows={3} defaultValue={message} onBlur={handleChange} />
-                </Form.Group>
+                  <Form.Group controlId="MessageInput">
+                      <h3 htmlFor="message">Message:</h3>
+                      <Form.Control id="messageValue" as="textarea" name="MessageInput" rows={3} defaultValue={message} onBlur={handleChange} />
+                  </Form.Group>
 
-                {errorMessage && (
-                <div>
-                    <p className="error-text">{errorMessage}</p>
-                </div>
-                )}
-                <Button data-testid="button" type="submit">Submit</Button>
-            </div>
-        </Form>
+                  {errorMessage && (
+                  <div>
+                      <p className="error-text">{errorMessage}</p>
+                  </div>
+                  )}
+                  <Button data-testid="button" type="submit">Submit</Button>
+              </div>
+          </Form>
+        </Card>
     </section>
   );
 }
